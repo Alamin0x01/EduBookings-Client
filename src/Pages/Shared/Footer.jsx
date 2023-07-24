d0f5aef07876250f5a49db6f1f4a477e566006f9
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-07-24 22:41:02 +0600

    update/edit profile and footer info added

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,4 +1,10 @@
 import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagramSquare,
+  FaGithub,
+} from "react-icons/fa";
 const Footer = () => {
   return (
     // <footer className="footer mt-3 p-10 bg-base-200 text-base-content">
@@ -29,7 +35,7 @@ const Footer = () => {
     //     <a className="link link-hover">Cookie policy</a>
     //   </div>
     // </footer>
-    <footer className=" p-10 bg-sky-500 text-white">
+    <footer className=" mt-5 p-10 bg-cyan-400 text-white">
       <div className="flex justify-between flex-col lg:flex-row items-center text-center lg:space-y-0 space-y-6">
         <div>
           <img
@@ -38,9 +44,9 @@ const Footer = () => {
             alt=""
           />
           <p>
-            Edu-Cademy
+            EduBookings
             <br />
-            support@Edu-Cademy.com <br />
+            support@EduBookings.com <br />
             +880 175634456
           </p>
         </div>
@@ -52,23 +58,18 @@ const Footer = () => {
             </Link>
           </p>
           <p>
-            <Link className="hover:text-orange" to="/Classes">
-              Classes
+            <Link className="hover:text-orange" to="/">
+              Colleges
             </Link>
           </p>
           <p>
             <Link className="hover:text-orange" to="/">
-              Teachers
+              Admissions
             </Link>
           </p>
           <p>
             <Link className="hover:text-orange" to="/">
-              Dashboard
-            </Link>
-          </p>
-          <p>
-            <Link className="hover:text-orange" to="/">
-              Blogs
+              My college
             </Link>
           </p>
         </div>
@@ -79,18 +80,34 @@ const Footer = () => {
             Bonani Road <br />
             Dhaka
             <br />
-            1st floor, Edu-Cademy
+            1st floor, EduBookings
           </p>
         </div>
         <div>
           <span className="footer-title">Social Links</span>
+          <div className="mb-4">
+            <div className="flex space-x-4">
+              <a href="https://www.facebook.com/EduBookings">
+                <FaFacebook className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://www.instagram.com/EduBookings/">
+                <FaInstagramSquare className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://twitter.com/EduBookings">
+                <FaTwitter className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+              <a href="https://github.com/EduBookings">
+                <FaGithub className="text-3xl cursor-pointer hover:text-orange" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div>
         <div className=" items-center mt-8">
           <div className="border-t border-orange mx-auto mb-6"></div>
           <div className="text-center">
-            <p>Copyright © Edu-Cademy - 2023 - All right reserved</p>
+            <p>Copyright © EduBookings - 2023 - All right reserved</p>
           </div>
         </div>
       </div>
