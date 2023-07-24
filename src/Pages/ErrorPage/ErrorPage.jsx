6eaa24ceaf7be547efa8911087ca4968268aa391
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-07-24 20:31:13 +0600

    error page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -12,18 +12,17 @@ const ErrorPage = () => {
       </Helmet>
       <section className="flex">
         <div className="container flex flex-col items-center justify-center">
-          {/* Lottie Animation */}
           <div className="relative lg:w-1/2 ">
-            <div className="w-full lg:w-4/5 lg:ml-auto h-56 sm:h-96">
+            <div className=" lg:w-11/12  sm:h-96">
               <Lottie animationData={reader} loop={true} />
             </div>
           </div>
-          <div className="text-center mt-52 md:pl-36">
+          <div className="text-center mt-32 mr-52 md:pl-36">
             <p className="text-2xl font-semibold md:text-3xl mt-5  text-red-600">
               {error?.message}
             </p>
             <Link to="/">
-              <button className="btn btn-secondary md:mt-5">
+              <button className="btn btn-outline btn-info md:mt-5">
                 Back to homepage
               </button>
             </Link>
